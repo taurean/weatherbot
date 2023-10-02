a2ecbb9a6e1f1f65994b1e61a54e8716fc15a3e4
Taurean Bryant <taurean@hey.com>
2023-10-01 22:35:06 -0700

    created the weather API services

--- src/services/WeatherService.tsx
+++ src/services/WeatherService.tsx
@@ -1,7 +1,7 @@
 const GEO_API_URL = `https://geocoding-api.open-meteo.com/v1/search?`;
-// const WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?`;
+const WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?`;
 
-interface LocationResponceItem {
+export interface LocationResponceItem {
   id: number;
   name: string;
   latitude: number;
@@ -10,13 +10,30 @@ interface LocationResponceItem {
   admin1: string;
 }
 
-interface LocationReponse {
-  results: LocationResponceItem[];
+export interface LocationReponse {
+  results: {
+    results: LocationResponceItem[];
+  };
 }
 
-interface ErrorResponse {
+export interface ErrorResponse {
   error: boolean;
   reason: string;
+}
+
+export interface WeatherResponse {
+  hourly: {
+    time: string[];
+    temperature_2m: number[];
+    precipitation_probability: number[];
+    weathercode: number[];
+  };
+  daily: {
+    time: string[];
+    weathercode: number[];
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+  };
 }
 
 export async function getLocation(
@@ -28,6 +45,9 @@ export async function getLocation(
     );
     if (response.ok) {
       const results = await response.json();
+      console.log(results.results[0].name);
+      console.log(results.results[0].latitude);
+      console.log(results.results[0].longitude);
       return { results };
     }
     if (response.status == 404 || response.status == 500) {
@@ -40,66 +60,29 @@ export async function getLocation(
   }
 }
 
-// function weatherCodeToString(code: number) {
-//   switch (code) {
-//     case 0:
-//       return "clear sky";
-//     case 1:
-//       return "mainly clear";
-//     case 2:
-//       return "partly cloudy";
-//     case 3:
-//       return "overcast";
-//     case 45:
-//     case 48:
-//       return "fog";
-//     case 51:
-//       return "light drizzle";
-//     case 53:
-//     case 55:
-//       return "moderate drizzle";
-//     case 56:
-//     case 57:
-//       return "freezing drizzle";
-//     case 61:
-//       return "slight rain";
-//     case 63:
-//       return "rain";
-//     case 65:
-//       return "heavy rain";
-//     case 66:
-//     case 67:
-//       return "freezing rain";
-//     case 71:
-//       return "light snow";
-//     case 73:
-//       return "snow";
-//     case 75:
-//     case 77:
-//       return "heavy snow";
-//     case 80:
-//       return "slight rain showers";
-//     case 81:
-//       return "rain showers";
-//     case 82:
-//       return "violent rain showers";
-//     case 85:
-//       return "snow showers";
-//     case 86:
-//       return "heavy snow showers";
-//     case 95:
-//       return "thunderstorm";
-//     case 96:
-//       return "thunderstorm with light hail";
-//     case 99:
-//       return "thunderstorm with heavy hail";
-//     default:
-//       return "unknown";
-//   }
-// }
-
-//api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=GMT
-
-// function getWeatherApiUrl(lat: number, long: number, timezone: string) {
-//   return `${WEATHER_API_URL}latitude=${lat}&longitutde=${long}&hourly=temperature_2m,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=${timezone}`;
-// }
+export async function getWeather(
+  latitude: number,
+  longitude: number
+): Promise<WeatherResponse | ErrorResponse> {
+  try {
+    const response = await fetch(
+      `${WEATHER_API_URL}latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`
+    );
+    if (response.ok) {
+      const apiResult = await response.json();
+      console.log(apiResult);
+      console.log(apiResult.hourly.temperature_2m[0]);
+      console.log(apiResult.daily.temperature_2m_max[0]);
+      console.log(apiResult.daily.temperature_2m_min[0]);
+      console.log(apiResult.hourly.weathercode[0]);
+      return apiResult;
+    }
+    if (response.status === 404 || response.status === 500) {
+      const error = await response.json();
+      return error;
+    }
+    throw new Error("network response was not ok");
+  } catch (error) {
+    return { error: true, reason: (error as Error).message };
+  }
+}
